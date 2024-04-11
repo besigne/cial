82c54d13b03cb9847f7c2735705310f12a6fc68a
matheus <mboesingsilva@gmail.com>
2024-04-11 14:43:03 -0300

    removed svgs saving process, adding more sites on website.txt for testing

--- Scrapper.py
+++ Scrapper.py
@@ -1,5 +1,4 @@
 import re
-import os
 import json
 import threading
 import requests
@@ -10,16 +9,13 @@ from requests.exceptions import RequestException
 
 class Scrapper(threading.Thread):
 
-    def __init__(self, url: str):
+    def __init__(self, url: str) -> None:
         super().__init__()
         self.url = url
         self.base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
-        aux = re.search(r'www\.(.*?)\.com', self.base_url)
-        self.name = aux.group(1)
         self.soup = None
-        self.result = None
 
-    def run(self):
+    def run(self) -> None:
         try:
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
@@ -31,21 +27,22 @@ class Scrapper(threading.Thread):
             page = session.get(self.url)
             if page.status_code == 200:
                 self.soup = BeautifulSoup(page.content, 'html.parser')
-                self.result = {
-                    "logo": self.find_logos(),
-                    "phones": self.find_phones(),
-                    "website": f"{self.url}",
-                }
-                print(json.dumps(self.result))
+                self.output(logo=self.find_logos(), phones=self.find_phones())
             else:
-                self.result = "Dead Page"
-                print(self.result)
+                print("Dead page")
         except RequestException:
-            print("Error: ", RequestException)
+            raise RequestException
 
-    def find_logos(self):
+    def output(self, logo: str, phones: list):
+        result = {
+            "logo": logo,
+            "phones": phones,
+            "website": self.url
+        }
+        print(json.dumps(result))
+
+    def find_logos(self) -> str:
         header = self.soup.find('header')
-        svgs = self.soup.find_all('svg')
         result = None
         if header:
             logos = header.find_all('img')
@@ -55,19 +52,12 @@ class Scrapper(threading.Thread):
             logos = self.soup.find_all('img')
             if logos:
                 result = self.treat_logos(logos)
-
-        if svgs:
-            self.save_svgs(svgs)
-
         return result
-
-    def find_phones(self):
-        pass
 
     def treat_logo_path(self, path):
         if path.startswith('//'):
             path = f'https:{path}'
-        if path.startswith('/local'):
+        if path.startswith('/'):
             path = f'{self.base_url}{path}'
         return path
 
@@ -83,14 +73,24 @@ class Scrapper(threading.Thread):
                         result.append(self.treat_logo_path(logo['src']))
         if len(result) > 0:
             return result[0]
-        return result
+        return ""
 
-    def save_svgs(self, svgs):
-        path = f"svgs/{self.name}/"
-        if not os.path.exists(path):
-            os.mkdir(path)
-        for index, svg in enumerate(svgs):
-            if index == 19:
-                print(svg)
-            with open(f"{path}{index}.svg", "w") as f:
-                f.write(str(svg))
+    def find_phones(self) -> list:
+        phones = []
+        possible_phones = str(self.soup)
+        patterns = [
+            r'\(\d{3}\) \d{3}-\d{4}',
+            r'\b\d{2} \d{2} \d{2}\b',
+            r'\+\d+ \d{1,3} \d{4} \d{4,}',
+            r'\b\d{4} \d{3} \d{3}\b',
+            r'\+\d+ \(\d{3}\) \d{3}-\d{2}-\d{2}',
+            r'\d{4} \d{6}'
+        ]
+        for pattern in patterns:
+            phones_found = re.findall(pattern, possible_phones)
+            for phone in phones_found:
+                phones.append(phone)
+
+        phones = list(set(phones))
+        phones = [number.replace('-', ' ').replace('/', ' ') for number in phones]
+        return phones
--- module.py
+++ module.py
@@ -1,23 +1,10 @@
 import sys
 from Scrapper import Scrapper
 
-lines = [
-    # "https://www.cmsenergy.com/contact-us/default.aspx",
-    # "https://www.illion.com.au",
-    # "https://www.phosagro.com/contacts",
-    # "https://www.powerlinx.com/contact",
-    "https://www.cialdnb.com/en,",
-    # "https://www.illion.com.au/contact-us/"
-]
-
 threads = []
 
-# for line in sys.stdin:
-#     thread = Scrapper(url=line.strip())
-#     thread.start()
-#     threads.append(thread)
-
-for line in lines:
+for line in sys.stdin:
     thread = Scrapper(url=line.strip())
     thread.start()
     threads.append(thread)
+
